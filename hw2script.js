812d81f7bc442fcd3d90f416716821606a6f8d03
Faz Kay <fazkay@Fazs-MacBook-Air.local>
2023-02-01 23:34:00 -0800

    HW-2 Part 3 Extra Point stable table without background color on the input field upon click

--- hw2script.js
+++ hw2script.js
@@ -112,3 +112,27 @@ function getData() {
 //     alert("Doesn't Work");
 //   }
 // };
+
+//-----Scripting the part-3 (Extra Credit) below-----
+
+// Sliding Panel below
+$(document).ready(function () {
+  $("#flip").click(function () {
+    $("#panel").slideToggle("slow");
+  });
+});
+
+function calculateExchange() {
+  let usd = document.getElementById("usd").value;
+  let euro = document.getElementById("euro");
+  let canadian = document.getElementById("canadian");
+  let hongkong = document.getElementById("hongkong");
+  let japanese = document.getElementById("japanese");
+  let mexican = document.getElementById("mexican");
+
+  euro.value = (usd * 0.84).toFixed(2);
+  canadian.value = (usd * 1.23).toFixed(2);
+  hongkong.value = (usd * 7.76).toFixed(2);
+  japanese.value = (usd * 110.8).toFixed(2);
+  mexican.value = (usd * 19.82).toFixed(2);
+}
