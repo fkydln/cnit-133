481c53eaad9ba05b03812399f6aa1fd2eac1b5cd
Faz Kay <fkydln@gmail.com>
2023-04-06 01:15:38 -0700

    part-1 mvp done.

--- hw5script.js
+++ hw5script.js
@@ -1,1 +1,22 @@
-function validateInputPart1() {}
+function validateInputPart1() {
+  let name = $("#part1name").val();
+  let radioCheck = $('input[name="ageGroup"]:checked').val();
+  let selectedCheckBoxes = [];
+  var dropdownValue = $("#movieGenre").val();
+
+  $("input[name='pt1browsers']:checked").each(function () {
+    selectedCheckBoxes.push($(this).val());
+  });
+
+  if (
+    name != "" &&
+    radioCheck != undefined &&
+    selectedCheckBoxes != "" &&
+    dropdownValue != "option"
+  ) {
+    $("#outputPt1").html("Thanks, your data was submitted!");
+    console.log("success!");
+  } else {
+    $("#outputPt1").html("Sorry, please fill the entire form completely.");
+  }
+}
