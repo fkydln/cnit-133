c5fa299c8749f5e141924c98be7951a638dd58a4
Faz Kay <fkydln@gmail.com>
2023-05-07 14:44:23 -0700

    Part-1 MVP is done.

--- hw7script.js
+++ hw7script.js
@@ -4,3 +4,29 @@ $(document).ready(function () {
     $(this).next(".accordion-content").slideToggle();
   });
 });
+
+$(document).ready(function () {
+  // Apply initial styles
+  applyStyles();
+
+  // Apply styles when form inputs change
+  $("#style-form input, #style-form select").change(function () {
+    applyStyles();
+  });
+
+  function applyStyles() {
+    var bg = $('input[name="bg-color"]:checked').val();
+    var underline = $('input[name="underline"]').prop("checked");
+    var bold = $('input[name="bold"]').prop("checked");
+    var italic = $('input[name="italic"]').prop("checked");
+    var fontSize = $('select[name="font-size"]').val();
+
+    $("#content").css("background-color", bg);
+    $("#content p").css({
+      "text-decoration": underline ? "underline" : "none",
+      "font-weight": bold ? "bold" : "normal",
+      "font-style": italic ? "italic" : "normal",
+      "font-size": fontSize,
+    });
+  }
+});
